632faecac0ed560a039dd371b2175381781e4291
Sam Wiseman <swiseman@seas.harvard.edu>
2017-01-25 16:19:45 -0500

    .

--- onmt/data/init.lua
+++ onmt/data/init.lua
@@ -7,5 +7,6 @@ data.BoxDataset = require('onmt.data.BoxDataset')
 data.BoxBatch = require('onmt.data.BoxBatch')
 data.BoxDataset2 = require('onmt.data.BoxDataset2')
 data.BoxBatch2 = require('onmt.data.BoxBatch2')
+data.BoxBatch3 = require('onmt.data.BoxBatch3')
 
 return data
